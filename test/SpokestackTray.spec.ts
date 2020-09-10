153e216c57ba584bb49dfef430db3aa8f62a99b8
Timmy Willison <4timmywil@gmail.com>
2020-09-10 16:16:12 -0400

    test: add new exports

--- test/SpokestackTray.spec.ts
+++ test/SpokestackTray.spec.ts
@@ -1,12 +1,26 @@
 import SpokestackTray, {
+  addListener,
+  checkSpeech,
+  isInitialized,
   isListening,
-  listen
+  isStarted,
+  listen,
+  removeListener,
+  requestSpeech,
+  stopListening
 } from 'react-native-spokestack-tray'
 
 describe('SpokestackTray', () => {
   it('should include the expected exports', () => {
     expect(SpokestackTray).toBeDefined()
     expect(listen).toBeDefined()
+    expect(stopListening).toBeDefined()
     expect(isListening).toBeDefined()
+    expect(isStarted).toBeDefined()
+    expect(isInitialized).toBeDefined()
+    expect(addListener).toBeDefined()
+    expect(removeListener).toBeDefined()
+    expect(checkSpeech).toBeDefined()
+    expect(requestSpeech).toBeDefined()
   })
 })
